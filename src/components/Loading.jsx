15099005b035368f765eec8aea55bda18a898bd6
contreras48 <contreras4893@gmail.com>
2026-04-28 17:59:53 -0600

    feat: add loading state component and update App to handle loading status

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+function Loading(){
+  return (
+    <div>
+      <h2 className="subtitle">
+        Fetching repository...     
+      </h2>
+      <p className="body">Scanning open-source repositories, please wait</p>
+    </div>
+  ); 
+}
+
+export default Loading
